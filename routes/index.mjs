4ffaf60583c7ca6e4a064ea6a8a80ec98f352366
goz4el <hordemzerado@gmail.com>
2021-10-10 00:37:34 -0400

    Making the home route use the AbstractStoreNotes API

--- routes/index.mjs
+++ routes/index.mjs
@@ -1,9 +1,18 @@
-import { default as express } from "express";
+import * as express from "express";
+import { NotesStore as notes } from "../app.mjs";
 export const router = express.Router();
 
 router.get("/", async (req, res, next) => {
-  //...placeholder for Notes home page code
-  res.render("index", { title: "Notes" });
+  try {
+    const keyList = await notes.keyList();
+    const keyPromises = keyList.map((key) => {
+      return notes.read(key);
+    });
+    const notelist = await Promise.all(keyPromises);
+    res.render("index", { title: "Notes", notelist });
+  } catch (err) {
+    next(err);
+  }
 });
 
 module.exports = router;
