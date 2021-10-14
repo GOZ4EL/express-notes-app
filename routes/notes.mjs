8a1b8d733861772898c4dbcadc7c9f4dcfc3adc6
goz4el <hordemzerado@gmail.com>
2021-10-14 09:06:56 -0400

    Adding the route handler for the create-update model functionality

--- routes/notes.mjs
+++ routes/notes.mjs
@@ -4,7 +4,7 @@ import { NotesStore as notes } from "../app.mjs";
 
 export const router = express.Router();
 
-// Add note.
+// Add note
 router.get("/add", (req, res, next) => {
   res.render("noteedit", {
     title: "Add a note",
@@ -12,4 +12,27 @@ router.get("/add", (req, res, next) => {
     notekey: "",
     note: undefined,
   });
+});
+
+// Save note(update)
+router.post("/save", async (req, res, next) => {
+  try {
+    let note;
+    if (req.body.docreate === "create") {
+      note = await notes.create(
+        req.body.notekey,
+        req.body.title,
+        req.body.body
+      );
+    } else {
+      note = await notes.update(
+        req.body.notekey,
+        req.body.title,
+        req.body.body
+      );
+    }
+    res.redirect("/notes/view/key?=" + req.body.notekey);
+  } catch (err) {
+    next(err);
+  }
 });
